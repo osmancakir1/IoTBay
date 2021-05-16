e6fdc0456d5ad536c40e509df75975690888bc7b
osmancakir1 <79437257+osmancakir1@users.noreply.github.com>
2021-05-16 16:50:45 +1000

    Update User.java

--- src/java/uts/isd/model/User.java
+++ src/java/uts/isd/model/User.java
@@ -2,17 +2,17 @@ package uts.isd.model;
 import java.io.Serializable;
 
 public class User implements Serializable {
-    
-    private int userId;
     private String email;
+    private String name;
     private String password;
+    private String phone;
+    
 
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
+    public User(String email, String name, String password, String phone) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+        this.name = phone; 
     }
 
     public String getEmail() {
@@ -22,6 +22,15 @@ public class User implements Serializable {
     public void setEmail(String email) {
         this.email = email;
     }
+    
+     public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 
     public String getPassword() {
         return password;
@@ -29,22 +38,13 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" + "userId=" + userId + ", email=" + email + ", password=" + password + '}';
-    }
-
-    public User() {
-    }
-
-    public User(int userId, String email, String password) {
-        this.userId = userId;
-        this.email = email;
-        this.password = password;
-    }
-
+    }   
     
-    
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }   
 }
